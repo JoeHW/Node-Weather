ece39103f3bae81df79603021598a56979638139
Joe Williamson <jhwilliamson@outlook.com>
2017-09-05 19:16:42 -0400

    Error handling

--- playground/app.js
+++ playground/app.js
@@ -22,7 +22,13 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + encodeAddress,
   json: true
 }, (error,response,body) => {
+  if(error){
+    console.log('Unable to reach server');
+  } else if(body.status === 'ZERO_RESULTS') {
+    console.log('Unable to find that address...');
+  } else if(body.status === 'OK'){
   console.log(`Address: ${body.results[0].formatted_address}`);
   console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
   console.log(`Longtitude: ${body.results[0].geometry.location.lng}`);
-})
+  }
+ })
